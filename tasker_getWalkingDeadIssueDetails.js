28742b56cb437a0bb8b70b2695215ee75bcd3790
Stephen Gregory <artcode08@gmail.com>
2015-01-20 23:12:46 -0330

    Variable name changes

--- tasker_getWalkingDeadIssueDetails.js
+++ tasker_getWalkingDeadIssueDetails.js
@@ -10,17 +10,17 @@ xhr.onload = function () {
   var availabilityText = doc.querySelector('div.vision-contentbox-content.tt-content-style-black > span').innerText;
   var dateStart = availabilityText.substr(availabilityText.indexOf(':') + 1).trim();
   var array = dateStart.split(' ');
-  var releasedatestring = array[0] + ' ' + array[1] + ' ' + array[2] + ' 09:00 GMT-0500';
+  var releaseDateString = array[0] + ' ' + array[1] + ' ' + array[2] + ' 09:00 GMT-0500';
 
-  var releaseDate = new Date(releasedatestring);
+  var releaseDate = new Date(releaseDateString);
 
-  var msUntil = releaseDate - new Date();
+  var millisecondsUntilNextIssue = releaseDate - new Date();
 
-  var days = msUntil/1000/60/60/24;
-  var wholedays = Math.floor(days);
-  var img = getIssueImagePath(issueNumber);
+  var days = millisecondsUntilNextIssue/1000/60/60/24;
+  var wholeDays = Math.floor(days);
+  var issueCoverImageURL = getIssueImagePath(issueNumber);
   
-  setVariablesInTasker(img, issueNumber, wholedays, releasedatestring);
+  setVariablesInTasker(issueCoverImageURL, issueNumber, wholeDays, releaseDateString);
 
   //leave the Tasker javascriptlet
   exit();
@@ -32,19 +32,19 @@ function getIssueImagePath(issueNo) {
     if (issueNo.length === 1) {
 	    issueNo = '0' + issueNo;
 	}
-	return ['http://i1.wp.com/walkingdeadcomics.org/wp-images/gallery/standard/', issueNo, '.jpg'].join()
+	return ['http://i1.wp.com/walkingdeadcomics.org/wp-images/gallery/standard/', issueNo, '.jpg'].join('')
 }
 
-function setVariablesInTasker(img, issueNumber, wholedays, releasedatestring) {
-  setLocalVariable('img',img);
+function setVariablesInTasker(issueCoverImageURL, issueNumber, wholeDays, releaseDateString) {
+  setLocalVariable('img',issueCoverImageURL);
   setLocalVariable('issueno',issueNumber);
-  setLocalVariable('days',wholedays);
-  setLocalVariable('date',releasedatestring);
+  setLocalVariable('days',wholeDays);
+  setLocalVariable('date',releaseDateString);
 }
 
 function setLocalVariable(name, value, debug) {
     if (debug) {
     flash('setting ' + name + ' to ' + value);
 	}
-	setLocal(name,value);
+	setLocal(name, value);
 };
