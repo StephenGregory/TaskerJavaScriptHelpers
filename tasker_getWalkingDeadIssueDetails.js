61f1b3fbd07ba13ac23981e1109950b52126ef7e
Stephen Gregory <artcode08@gmail.com>
2015-01-21 22:39:34 -0330

    TWD variable renaming and refactoring

--- tasker_getWalkingDeadIssueDetails.js
+++ tasker_getWalkingDeadIssueDetails.js
@@ -3,7 +3,7 @@ xhr.open('GET', 'http://walkingdeadcomics.org/the-walking-dead-next-issue/', tru
 
 xhr.onload = function () {
   var parser = new DOMParser();
-  var doc = parser.parseFromString(xhr.responseText, 'text/html');
+  var doc = parser.parseFromString(this.responseText, 'text/html');
 
   var titleNode = doc.getElementById('p-title');
   var issueNumber = titleNode.innerText.substr(titleNode.innerText.indexOf('#')+1);
@@ -13,38 +13,42 @@ xhr.onload = function () {
   var releaseDateString = array[0] + ' ' + array[1] + ' ' + array[2] + ' 09:00 GMT-0500';
 
   var releaseDate = new Date(releaseDateString);
-
-  var millisecondsUntilNextIssue = releaseDate - new Date();
-
-  var days = millisecondsUntilNextIssue/1000/60/60/24;
-  var wholeDays = Math.floor(days);
-  var issueCoverImageURL = getIssueImagePath(issueNumber);
+  var wholeDaysUntilNextIssue = getDaysUntilDate(releaseDate);
+  var issueCoverImageURL = generateIssueCoverURL(issueNumber);
   
-  setVariablesInTasker(issueCoverImageURL, issueNumber, wholeDays, releaseDateString);
+  setComicVariablesInTasker(issueCoverImageURL, issueNumber, wholeDaysUntilNextIssue, releaseDateString);
 
-  //leave the Tasker javascriptlet
+  //signal to Tasker that the code is completed
   exit();
 };
 
 xhr.send();
 
-function getIssueImagePath(issueNo) {
+function getDaysUntilDate(date) {
+  var millisecondsUntilNextIssue = date - new Date();
+  var days = millisecondsUntilNextIssue/1000/60/60/24;
+  var wholeDaysUntilNextIssue = Math.floor(days);
+  
+  return wholeDaysUntilNextIssue;
+}
+
+function generateIssueCoverURL(issueNo) {
     if (issueNo.length === 1) {
 	    issueNo = '0' + issueNo;
 	}
 	return ['http://i1.wp.com/walkingdeadcomics.org/wp-images/gallery/standard/', issueNo, '.jpg'].join('')
 }
 
-function setVariablesInTasker(issueCoverImageURL, issueNumber, wholeDays, releaseDateString) {
+function setComicVariablesInTasker(issueCoverImageURL, issueNumber, wholeDaysUntilNextIssue, releaseDateString) {
   setLocalVariable('img',issueCoverImageURL);
   setLocalVariable('issueno',issueNumber);
-  setLocalVariable('days',wholeDays);
+  setLocalVariable('days',wholeDaysUntilNextIssue);
   setLocalVariable('date',releaseDateString);
 }
 
 function setLocalVariable(name, value, debug) {
     if (debug) {
-    flash('setting ' + name + ' to ' + value);
+        flash('setting ' + name + ' to ' + value);
 	}
 	setLocal(name, value);
 };
