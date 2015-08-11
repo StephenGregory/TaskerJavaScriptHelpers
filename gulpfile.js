5c4c7bafbf5745ee40c37d2cc7a0374308f699d9
Stephen Gregory <artcode08@gmail.com>
2015-08-11 19:47:53 -0230

    Add eslint gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 /*eslint-env node */
 var gulp = require('gulp');
 var strip = require('gulp-strip-comments');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function () {
+gulp.task('default', ['lint'], function () {
     return gulp.src('./src/**/*.js')
         .pipe(strip({line: true}))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', function () {
+    return gulp.src('./src/**/*.js')
+        .pipe(eslint({useEslintrc: true}))
+        .pipe(eslint.format(''))
+        .pipe(eslint.failOnError());
 });
