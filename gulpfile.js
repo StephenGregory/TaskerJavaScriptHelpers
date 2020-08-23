050fb9f4469eaa0dcd273b3893f9c294839e1258
Stephen Gregory <stephengregorynl@gmail.com>
2020-08-23 13:33:03 -0400

    Upgrade Gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 /*eslint-env node */
-var gulp = require('gulp');
-var strip = require('gulp-strip-comments');
-var eslint = require('gulp-eslint');
+const strip = require('gulp-strip-comments');
+const eslint = require('gulp-eslint');
 
-gulp.task('default', ['lint'], function () {
-    return gulp.src('./src/**/*.js')
-        .pipe(strip({line: true}))
-        .pipe(gulp.dest('dist'));
-});
+const { src, dest, series } = require('gulp');
 
-gulp.task('lint', function () {
-    return gulp.src('./src/**/*.js')
-        .pipe(eslint({useEslintrc: true}))
+function clean(cb) {
+    return src('./src/**/*.js')
+        .pipe(strip({ line: true }))
+        .pipe(dest('dist'));
+}
+
+function lint(cb) {
+    return src('./src/**/*.js')
+        .pipe(eslint({ useEslintrc: true }))
         .pipe(eslint.format(''))
         .pipe(eslint.failOnError());
-});
+}
+
+exports.default = series(lint, clean);
