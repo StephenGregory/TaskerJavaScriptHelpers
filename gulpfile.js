7a75fcebfa7ab0aff361ff4a4029163ffc4c0d0d
Stephen Gregory <stephengregorynl@gmail.com>
2020-08-24 12:26:39 -0400

    Refactor Tasker to utility class

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
-/*eslint-env node */
 const strip = require('gulp-strip-comments');
 const eslint = require('gulp-eslint');
-
+const stripDebug = require('gulp-strip-debug');
 const { src, dest, series } = require('gulp');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const glob = require('glob');
+const eventStream = require('event-stream');
+const buffer = require('vinyl-buffer');
 
-function clean(cb) {
-    return src('./src/**/*.js')
-        .pipe(strip({ line: true }))
-        .pipe(dest('dist'));
-}
-
-function lint(cb) {
-    return src('./src/**/*.js')
+function lint() {
+    return src('./src/**.js')
         .pipe(eslint({ useEslintrc: true }))
         .pipe(eslint.format(''))
-        .pipe(eslint.failOnError());
+        .pipe(eslint.failAfterError());
 }
 
-exports.default = series(lint, clean);
+function taskerfy(done) {
+    glob('**/tasker*js', { ignore: '!src/tasker*' }, function (err, files) {
+        if (err) {
+            done(err);
+        }
+        var tasks = files.map(function (entry) {
+            return browserify({ entries: [entry] })
+                .bundle()
+                .pipe(source(entry))
+                .pipe(buffer())
+                .pipe(strip({ line: true }))
+                .pipe(stripDebug())
+                .pipe(dest('dist'));
+        });
+        eventStream.merge(tasks).on('end', done);
+    });
+}
+
+exports.lint = lint;
+exports.taskerfy = taskerfy;
+exports.default = series(lint, taskerfy);
